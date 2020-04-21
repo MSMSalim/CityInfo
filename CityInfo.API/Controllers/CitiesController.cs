95b32a39be31a9bae8e72d337a6b6136c040013d
MSMSalim <salim.belim@moneysupermarket.com>
2020-04-21 23:18:59 +0100

    api solution created and returns city from inmemory object

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    //[Route("api/[controller]")]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        public IActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+           var city =  CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == id);
+
+           if (city == null)
+           {
+              return NotFound();
+           }
+
+            return Ok(city);
+        }
+    }
+}
---
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetPointsOfInterest(int cityId, int id)
+        {
+            var city = CitiesDataStore.Current.Cities
+                .FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            //find point of interest
+            var pointOfInterest = city.PointsOfInterest
+                .FirstOrDefault(x => x.Id == id);
+
+            if (pointOfInterest == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pointOfInterest);
+        }
+    }
+}
